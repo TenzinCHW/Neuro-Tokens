31507348db9047a7e6d7072a1eacfa9a4b648f82
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2024-05-01 21:18:03 -0700

    Added small script for creating directories

---
+++ scripts/createdatadir.sh
@@ -0,0 +1,4 @@
+mkdir -p ../data/exp_pro/matrix/blanche/full/{cdmentropy,complete,null}
+mkdir ../data/exp_pro/matrix/blanche/full/cdmentropy/{input,complete,null}
+mkdir -p ../data/exp_pro/matrix/joost_long/full/{cdmentropy,complete,null}
+mkdir ../data/exp_pro/matrix/joost_long/full/cdmentropy/{input,complete,null}
