711f5fdff99687feba74f3388102574c1bf4dfee
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2024-05-14 04:01:47 -0700

    Updated code for plotting heatmap

--- scripts/plot_heatmap.jl
+++ scripts/plot_heatmap.jl
@@ -6,18 +6,26 @@ import Plots
 function save_plot_hm(hm, loc, min_val, max_val)
     hm = sum(hm, dims=3) / size(hm)[3]
     hm = hm[:, :, 1]
-    Plots.heatmap(10:5:50, 500:500:5000, hm, xlabel="window size / number of timesteps",  ylabel="bin size / μs", clim=(min_val, max_val))
+    Plots.heatmap(10:5:60, 500:500:6000, hm, xlabel="window size / number of timesteps",  ylabel="bin size / μs", clim=(min_val, max_val))
     Plots.savefig(loc)
 end
 
 
-data = DrWatson.wload(DrWatson.datadir("entropy_winbin.jld2"))
-ext = "$(ARGS[1])"
-raw_ent = data["raw_entropy"]
-model_ent = data["model_entropy"]
-min_val = min(min(raw_ent...), min(model_ent...))
-max_val = max(max(raw_ent...), max(model_ent...))
-loc = DrWatson.plotsdir("raw_entropy.$ext")
+fpath = ARGS[1]
+data = DrWatson.wload(DrWatson.datadir(fpath))
+ext = ARGS[2]
+fn = split(fpath, "/")[end]
+f = split(fn, ".")[1]
+est_type = "MM correction"
+data = data[est_type]
+raw_ent = data["raw"]
+model_ent = data["complete"]
+null_ent = data["null"]
+min_val = min(min(raw_ent...), min(model_ent...), min(null_ent...))
+max_val = max(max(raw_ent...), max(model_ent...), max(null_ent...))
+loc = DrWatson.plotsdir("$(f)_raw.$ext")
 save_plot_hm(raw_ent, loc, min_val, max_val)
-loc = DrWatson.plotsdir("model_entropy.$ext")
+loc = DrWatson.plotsdir("$(f)_model.$ext")
 save_plot_hm(model_ent, loc, min_val, max_val)
+loc = DrWatson.plotsdir("$(f)_null.$ext")
+save_plot_hm(null_ent, loc, min_val, max_val)
